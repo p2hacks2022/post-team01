4caace5a8a96eee7c02f5e3c412990b561a5d890
mohrn3 <b1020230@fun.ac.jp>
2022-12-12 20:29:28 +0900

    Initial Commit

---
+++ TeddyBear2022/TeddyBear2022App.swift
@@ -0,0 +1,17 @@
+//
+//  TeddyBear2022App.swift
+//  TeddyBear2022
+//
+//  Created by 姜南圭 on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct TeddyBear2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeddyBear2022UITests/TeddyBear2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TeddyBear2022UITestsLaunchTests.swift
+//  TeddyBear2022UITests
+//
+//  Created by 姜南圭 on 2022/12/12.
+//
+
+import XCTest
+
+final class TeddyBear2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
